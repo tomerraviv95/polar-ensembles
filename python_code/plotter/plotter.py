b9526a0433157f00b1e13b0c9841651dddb8857b
tomer raviv <tomerraviv95@gmail.com>
2021-04-01 18:38:25 +0300

    Working version, but still checking for bugs

--- python_code/plotter/plotter.py
+++ python_code/plotter/plotter.py
@@ -1,3 +1,5 @@
+import datetime
+
 from python_code.plotter.plotter_types import get_polar_64_32, get_weighted_polar_64_32, get_polar_128_64, \
     get_weighted_polar_128_64, get_polar_1024_512, get_weighted_polar_1024_512, get_polar_2048_1024, \
     get_weighted_polar_2048_1024
@@ -5,7 +7,7 @@ from python_code.trainers.polar_fg_trainer import PolarFGTrainer
 from python_code.utils.python_utils import load_pkl, save_pkl
 from python_code.trainers.trainer import Trainer
 from python_code.plotter.plotter_config import *
-from dir_definitions import PLOTS_DIR
+from dir_definitions import PLOTS_DIR, FIGURES_DIR
 import matplotlib.pyplot as plt
 from globals import CONFIG
 import numpy as np
@@ -50,17 +52,17 @@ class Plotter:
                  label=graph_params['label'],
                  color=graph_params['color'],
                  marker=graph_params['marker'])
-        plt.title('FER Comparison of Different Polar Codes')
+        plt.title(f'{self.type} Comparison of Different Polar Codes')
         plt.yscale('log')
         plt.xlabel("Eb/N0(dB)")
-        plt.ylabel("FER")
+        plt.ylabel(self.type)
         plt.grid(True, which='both')
         plt.legend(loc='lower left', prop={'size': 15})
         plt.xlim((val_SNRs[0] - 0.5, val_SNRs[-1] + 0.5))
 
 
 if __name__ == '__main__':
-    plotter = Plotter(run_over=False, type='FER')
+    plotter = Plotter(run_over=False, type='BER')
     plotter.plot(*get_polar_64_32())
     plotter.plot(*get_weighted_polar_64_32())
     plotter.plot(*get_polar_128_64())
@@ -69,4 +71,10 @@ if __name__ == '__main__':
     plotter.plot(*get_weighted_polar_1024_512())
     plotter.plot(*get_polar_2048_1024())
     plotter.plot(*get_weighted_polar_2048_1024())
+    # path for the saved figure
+    current_day_time = datetime.datetime.now()
+    folder_name = f'{current_day_time.month}-{current_day_time.day}-{current_day_time.hour}-{current_day_time.minute}'
+    if not os.path.isdir(os.path.join(FIGURES_DIR, folder_name)):
+        os.makedirs(os.path.join(FIGURES_DIR, folder_name))
+    plt.savefig(os.path.join(FIGURES_DIR, folder_name, 'figure.png'), bbox_inches='tight')
     plt.show()
